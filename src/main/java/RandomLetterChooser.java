3103bfb84958fd6e2fde60d3948ba66209d63b57
OLGUO <143014722+OLGUO@users.noreply.github.com>
2024-04-22 09:52:41 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -4,30 +4,7 @@ public class RandomLetterChooser extends RandomStringChooser
 	public RandomLetterChooser(String str)
 	{ 
 		/* to be implemented in part (b) */
-		private int minSteps, totalSteps;
-		private int totalDays, actDays;
-
-		public StepTracker(int goal){
-			minSteps = goal;
-			totalSteps = 0; 
-			totalDays = 0;
-			actDays = 0;
-		}
-		public void addDailySteps(int steps){
-			totalSteps += steps;
-			totalDays++;
-			if(steps >= minSteps)
-				actDays++;
-		}
-		public int activeDays(){
-			return actDays;
-		}
-		public double averageSteps(){
-			if(totalDays == 0)
-				return 0.0;
-			else
-				return (double)totalSteps / totalDays;
-		}
+		super(getSingleLetters(str));
 	}
 	
 	
